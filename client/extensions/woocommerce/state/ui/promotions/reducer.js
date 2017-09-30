ebeb03dd37e8df90fa52f52d401eba05101e043a
Kevin Killingsworth <kevin@redfenix.com>
2017-09-30 02:23:14 -0500

    Store Promotions: Add edit state
    This adds edit state for promotion editing within the Store interface.

--- client/extensions/woocommerce/state/ui/promotions/reducer.js
+++ client/extensions/woocommerce/state/ui/promotions/reducer.js
@@ -2,9 +2,11 @@
  * Internal dependencies
  */
 import { combineReducers } from 'state/utils';
+import edits from './edits-reducer';
 import list from './list-reducer';
 
 export default combineReducers( {
+	edits,
 	list,
 } );
 
